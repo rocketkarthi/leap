117a6aa609f0758020e8b9a3882aad13629c5fb0
rocketkarthi <karthirajamanikam1203@gmail.com>
2024-06-24 17:05:54 +0530

    first commit

---
+++ ero/leap year.js
@@ -0,0 +1,11 @@
+
+let  year=prompt("leap year")
+if ((year%4==0)&&(year%100!=0)) {
+    console.log(year +"is a leap year");
+}
+// else if(year%400==0){
+//     console.log(year+"is a leap year");
+// }
+else{
+    console.log(year+"is not a leap year");
+}
